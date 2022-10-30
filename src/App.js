1ed56743b425b33d299ffd3edbfd08ff9b0097b0
romigh94 <romina.ghaderi@outlook.com>
2022-10-30 19:13:01 +0100

    klar med listitems, filtrering och highlighting

--- src/App.js
+++ src/App.js
@@ -1,13 +1,74 @@
 import './App.css';
-import React from "react";
+import React, { useState } from "react";
 
-function App() {
+export default function App() {
+  
+  const [list, setList] = useState([])
+  const [input, setInput] = useState('')
+
+  const newInput = {
+    inputValue: input
+  }
+
+  const filtered = list.filter((values) => {
+    return values.inputValue.toLowerCase().includes(input.toLowerCase()) 
+  }) 
+
+const handleSubmit = (e) => {
+  e.preventDefault()
+
+  setList([...list, newInput])
+  setInput("")
+}
+
 
   return (
-    <>
-      Change me
-    </>
+
+    <div>
+        <div className="list">
+          <form onSubmit={handleSubmit}>
+
+              <input type="text" 
+              className="input" 
+              placeholder="Enter word..." 
+              value={input} 
+              onChange={(e) => setInput(e.target.value)}
+              />
+
+          </form>
+
+            {filtered.map((result, index) => {
+
+                let startString = result.inputValue.substr(0,
+                  result.inputValue.toLowerCase().indexOf(input.toLowerCase())
+                );
+
+                let endString = result.inputValue.substr(
+                  result.inputValue.toLowerCase().indexOf(input.toLowerCase()) +
+                  input.length
+                );
+                            
+                let highlight = result.inputValue.substr(
+                  result.inputValue.toLowerCase().indexOf(input.toLowerCase()),
+                  input.length
+                );
+
+              return (  
+                <div key={index}>
+                  <li className="box">
+                      {startString}
+                        <strong>
+                          {highlight}
+                        </strong>
+                      {endString}
+                  </li>
+                </div>
+              )
+            })}
+      </div>
+</div>
+
   );
 }
 
-export default App;
+
